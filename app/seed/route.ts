e8aa4a7c37aebfee2a330567da3b25e3d6664b6f
Eduardo C Pacheco <pastor126@gmail.com>
2024-07-04 14:00:27 +0100

    Inativando arquivos não necessários

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,52 +1,52 @@
-// import bcrypt from 'bcrypt';
+// // import bcrypt from 'bcrypt';
 
 
-import { invoices, customers, revenue, users } from '../lib/placeholder-data';
+// import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-export async function handler(req: NextRequest) {
-  // Lógica para processar a requisição
-  const responseData = { message: 'Hello from seed route!' };
+// export async function handler(req: NextRequest) {
+//   // Lógica para processar a requisição
+//   const responseData = { message: 'Hello from seed route!' };
 
-  // Retornar uma resposta
-  return 
-
-
-const client = null;
-// await db.connect();
-
-async function seedUsers() {
+//   // Retornar uma resposta
+//   return 
 
 
-  const insertedUsers = await Promise.all(
-    users.map(async (user) => {
+// const client = null;
+// // await db.connect();
+
+// async function seedUsers() {
+
+
+//   const insertedUsers = await Promise.all(
+//     users.map(async (user) => {
  
-    }),
-  );
+//     }),
+//   );
 
-  return insertedUsers;
-}
+//   return insertedUsers;
+// }
 
-async function seedInvoices() {
+// async function seedInvoices() {
  
-}
+// }
 
-async function seedCustomers() {
+// async function seedCustomers() {
  
-}
+// }
 
-async function seedRevenue() {
+// async function seedRevenue() {
  
-}
+// }
 
-return async function GET() {
-  try {
+// return async function GET() {
+//   try {
    
-  } catch (err) {
-    console.error('Database Error:', err);
-    throw new Error('Falha app/seed/route.ts/seedInvoices/seedCustomers/seedRevenue/get');
+//   } catch (err) {
+//     console.error('Database Error:', err);
+//     throw new Error('Falha app/seed/route.ts/seedInvoices/seedCustomers/seedRevenue/get');
     
-  }
-}
-}
+//   }
+// }
+// }
