681906d2d055d64a3ea7dba7410b05bb22856304
Eduardo C Pacheco <pastor126@gmail.com>
2024-07-06 19:35:43 +0100

    seed

---
+++ app/seed/route.ts
@@ -0,0 +1,16 @@
+// import bcrypt from 'bcrypt';
+
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function handler(req: NextRequest) {
+  // Lógica para processar a requisição
+  const responseData = { message: 'Hello from seed route!' };
+
+  // Retornar uma resposta
+  return 
+
+const client = null;
+// await db.connect();
+
+}
+
