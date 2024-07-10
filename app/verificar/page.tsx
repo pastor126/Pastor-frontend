22d741db3769db1778274e5bcb85690055790625
Eduardo C Pacheco <pastor126@gmail.com>
2024-07-10 15:18:46 +0100

    Inserido página de verificação de email.

--- app/verificar/page.tsx
+++ app/verificar/page.tsx
@@ -1,1 +1,38 @@
-<div>Verifique seu email</div>
+'use client';
+import Image from 'next/image';
+
+
+export default function Page() {
+
+ 
+
+  return (
+    <>
+<main className="grid grid-flow-row auto-rows-auto flex items-center justify-center mt-6 p-10 bg-emerald-100">
+
+  <div className= "flex items-center justify-center p-1 md:w-50 ">
+      <Image
+        src="/PSarLogo.png"
+        width={100}
+        height={76}
+        className="hidden md:block"
+        alt="Imagem para desktop version"
+      />
+      <Image
+        src="/PSarLogo.png"
+        width={100}
+        height={76}
+        className="block md:hidden"
+        alt="Imagem para mobile version"
+      />
+      
+    </div>
+<div className= "flex items-center justify-center mb-20 md:w-50">
+      <h1>Verifique seu email para validar sua conta.</h1>
+      </div>
+     </main>
+      
+    </>
+    
+  );
+}
